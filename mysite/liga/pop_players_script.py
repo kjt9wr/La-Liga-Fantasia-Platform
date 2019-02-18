156d6506affd9108a16503df916c1bfc79460bbc
Kevin Thumim <kevinthumim@Kevins-MacBook-Pro.local>
2019-02-18 15:59:46 -0500

    loaded players

---
+++ mysite/liga/pop_players_script.py
@@ -0,0 +1,631 @@
+from liga.models import Player
+p = Player(name= 'Saquon Barkley', price= 110, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Ezekiel Elliot', price= 63, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Devonta Freeman', price= 62, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'LeSean Mccoy', price= 62, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Todd Gurley', price= 62, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Jordan Howard', price= 61, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Leonard Fournette', price= 57, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'AJ Green', price= 50, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Antonio Brown', price= 50, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Julio Jones', price= 50, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Michael Thomas', price= 50, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Odell Beckham', price= 50, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'TY Hilton', price= 50, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'David Johnson', price= 48, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'LeVeon Bell', price= 48, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Melvin Gordon', price= 48, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Christian McCaffrey', price= 45, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Mike Evans', price= 42, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Royce Freeman', price= 42, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Lamar Miller', price= 37, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Keenan Allen', price= 33, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Amari Cooper', price= 32, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Demaryius Thomas', price= 32, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Davante Adams', price= 31, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Doug Baldwin', price= 30, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Peyton Barber', price= 30, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Deandre Hopkins', price= 28, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Derrick Henry', price= 28, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Brandin Cooks', price= 26, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Corey Davis', price= 25, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Dalvin Cook', price= 25, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Kerryon Johnson', price= 25, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Stefon Diggs', price= 24, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Tyreek Hill', price= 24, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Travis Kelce', price= 24, position = 'TE', kept = True )
+
+p.save()
+p = Player(name= 'Joe Mixon', price= 24, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Chris Carson', price= 22, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Adam Thielen', price= 21, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Allen Robinson', price= 21, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Aaron Rodgers', price= 21, position = 'QB', kept = True )
+
+p.save()
+p = Player(name= 'Tom Brady', price= 21, position = 'QB', kept = True )
+
+p.save()
+p = Player(name= 'Jamison Crowder', price= 20, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Jarvis Landry', price= 19, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Robert Woods', price= 19, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Rob Gronkowski', price= 19, position = 'TE', kept = True )
+
+p.save()
+p = Player(name= 'Derrius Guice', price= 19, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Jamaal Williams', price= 19, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Christian Kirk', price= 18, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Larry Fitzgerald', price= 18, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Pierre Garcon', price= 18, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Marshawn Lynch', price= 18, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Rashaad Penny', price= 18, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Devin Funchess', price= 16, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Robby Anderson', price= 16, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Greg Olsen', price= 16, position = 'TE', kept = True )
+
+p.save()
+p = Player(name= 'Jay Ajayi', price= 16, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Golden Tate', price= 15, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Adrian Peterson', price= 15, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Emmanuel Sanders', price= 14, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Josh Doctson', price= 14, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Will Fuller', price= 14, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Kareem Hunt', price= 14, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Ronald Jones', price= 13, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Sony Michel', price= 13, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Chris Hogan', price= 12, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Josh Gordon', price= 12, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'JuJu Smith-Schuster', price= 12, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Julian Edelman', price= 12, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Kenny Golladay', price= 12, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Marquise Goodwin', price= 12, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Marvin Jones', price= 12, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Sammy Watkins', price= 12, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Jimmy Graham', price= 12, position = 'TE', kept = True )
+
+p.save()
+p = Player(name= 'Zach Ertz', price= 12, position = 'TE', kept = True )
+
+p.save()
+p = Player(name= 'Alex Collins', price= 12, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Alvin Kamara', price= 12, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Chris Thompson', price= 12, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Dion Lewis', price= 12, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Jerick McKinnon', price= 12, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Kenyan Drake', price= 12, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Mark Ingram', price= 12, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Rex Burkhead', price= 12, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Tevin Coleman', price= 12, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Russell Wilson', price= 12, position = 'QB', kept = True )
+
+p.save()
+p = Player(name= 'Adam Humphries', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Albert Wilson', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Allen Hurns', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Alshon Jeffery', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Anthony Miller', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Bruce Ellington', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Calvin Ridley', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Cameron Meredith', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Chris Conley', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Chris Godwin', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Cole Beasley', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Cooper Kupp', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Courtland Sutton', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Curtis Samuel', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'DaeSean Hamilton', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Danny Amendola', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Dante Pettis', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'DeSean Jackson', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Devante Parker', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Dez Bryant', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'DJ Moore', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Donte Moncrief', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Geronimo Allison', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'John Ross', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Jordan Matthews', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Jordy Nelson', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Keelan Cole', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Keke Coutee', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Kelvin Benjamin', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Kenny Stills', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Martavis Bryant', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Maurice Harris', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Michael Crabtree', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Michael Gallup', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Mike Williams', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Mohamed Sanu', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Nelson Agholor', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Paul Richardson', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Randall Cobb', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Rishard Matthews', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Robert Foster', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Ryan Grant', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'John Brown', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Sterling Shepard', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Sterling Shepherd', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Taylor Gabriel', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'TreQuan Smith', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Tyler Boyd', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Tyler Lockett', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Tyrell Williams', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Willie Snead', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Zay Jones', price= 10, position = 'WR', kept = True )
+
+p.save()
+p = Player(name= 'Austin Hooper', price= 10, position = 'TE', kept = True )
+
+p.save()
+p = Player(name= 'Benjamin Watson', price= 10, position = 'TE', kept = True )
+
+p.save()
+p = Player(name= 'Cameron Brate', price= 10, position = 'TE', kept = True )
+
+p.save()
+p = Player(name= 'David Njoku', price= 10, position = 'TE', kept = True )
+
+p.save()
+p = Player(name= 'Delanie Walker', price= 10, position = 'TE', kept = True )
+
+p.save()
+p = Player(name= 'Eric Ebron', price= 10, position = 'TE', kept = True )
+
+p.save()
+p = Player(name= 'Evan Engram', price= 10, position = 'TE', kept = True )
+
+p.save()
+p = Player(name= 'George Kittle', price= 10, position = 'TE', kept = True )
+
+p.save()
+p = Player(name= 'Jack Doyle', price= 10, position = 'TE', kept = True )
+
+p.save()
+p = Player(name= 'Jared Cook', price= 10, position = 'TE', kept = True )
+
+p.save()
+p = Player(name= 'Jordan Reed', price= 10, position = 'TE', kept = True )
+
+p.save()
+p = Player(name= 'Kyle Rudolph', price= 10, position = 'TE', kept = True )
+
+p.save()
+p = Player(name= 'OJ Howard', price= 10, position = 'TE', kept = True )
+
+p.save()
+p = Player(name= 'Trey Burton', price= 10, position = 'TE', kept = True )
+
+p.save()
+p = Player(name= 'Tyler Higbee', price= 10, position = 'TE', kept = True )
+
+p.save()
+p = Player(name= 'Vance McDonald', price= 10, position = 'TE', kept = True )
+
+p.save()
+p = Player(name= 'Aaron Jones', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Alfred Blue', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Austin Ekeler', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Bilal Powell', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Carlos Hyde', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Chris Ivory', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'CJ Anderson', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'DOnta Foreman', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Damien Williams', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Doug Martin', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Duke Johnson', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Elijah McGuire', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Frank Gore', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Giovani Bernard', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Gus Edwards', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Isaiah Crowell', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'James Conner', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'James White', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Javorius Allen', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Jaylen Samuels', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'John Kelly', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Jordan Wilkins', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Josh Adams', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Justin Davis', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Justin Jackson', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Kalen Ballage', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Latavious Murray', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Marlon Mack', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Matt Breida', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Nick Chubb', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Nyheim Hines', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Philip Lindsay', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Rob Kelley', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Ronald Jones', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Spencer Ware', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Tarik Cohen', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'TJ Yeldon', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Ty Montgomery', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Wendell Smallwood', price= 10, position = 'RB', kept = True )
+
+p.save()
+p = Player(name= 'Alex Smith', price= 10, position = 'QB', kept = True )
+
+p.save()
+p = Player(name= 'Andrew Luck', price= 10, position = 'QB', kept = True )
+
+p.save()
+p = Player(name= 'Baker Mayfield', price= 10, position = 'QB', kept = True )
+
+p.save()
+p = Player(name= 'Ben Roethlisberger', price= 10, position = 'QB', kept = True )
+
+p.save()
+p = Player(name= 'Cam Newton', price= 10, position = 'QB', kept = True )
+
+p.save()
+p = Player(name= 'Carson Wentz', price= 10, position = 'QB', kept = True )
+
+p.save()
+p = Player(name= 'Deshaun Watson', price= 10, position = 'QB', kept = True )
+
+p.save()
+p = Player(name= 'Drew Brees', price= 10, position = 'QB', kept = True )
+
+p.save()
+p = Player(name= 'Jared Goff', price= 10, position = 'QB', kept = True )
+
+p.save()
+p = Player(name= 'Josh Allen', price= 10, position = 'QB', kept = True )
+
+p.save()
+p = Player(name= 'Kirk Cousins', price= 10, position = 'QB', kept = True )
+
+p.save()
+p = Player(name= 'Lamar Jackson', price= 10, position = 'QB', kept = True )
+
+p.save()
+p = Player(name= 'Matt Ryan', price= 10, position = 'QB', kept = True )
+
+p.save()
+p = Player(name= 'Matt Stafford', price= 10, position = 'QB', kept = True )
+
+p.save()
+p = Player(name= 'Mitchell Trubisky', price= 10, position = 'QB', kept = True )
+
+p.save()
+p = Player(name= 'Patrick Mahomes', price= 10, position = 'QB', kept = True )
+
+p.save()
+p = Player(name= 'Philip Rivers', price= 10, position = 'QB', kept = True )
+
+p.save()
+p = Player(name= 'Darius Leonard', price= 10, position = 'D', kept = True )
+
+p.save()
+p = Player(name= 'Luke Kuechly', price= 10, position = 'D', kept = True )
+
+p.save()
