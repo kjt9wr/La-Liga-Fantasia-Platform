670a60601af2f8d34027fdb2cf2d7b7da64aef2a
Kevin Thumim <kevinthumim@Kevins-MacBook-Pro.local>
2019-02-19 20:38:39 -0500

    remaining cap works

--- mysite/liga/views.py
+++ mysite/liga/views.py
@@ -19,13 +19,22 @@ def rosters(request):
     #owner_list = Owner.objects.all()
     #player_list = Player.objects.filter(kept=True)
     roster_items = Roster.objects.all()
+
     kevin = Roster.objects.filter(owner__name='Kevin')
+    kevin_mcap = Owner.objects.values('cap')[0]['cap']
+    temp = Roster.objects.filter(owner__name='Kevin').filter(athlete__kept=True)
+    sum = 0
+    for item in temp:
+        sum = sum + item.athlete.price
+    kevin_rcap = kevin_mcap - sum
     template = loader.get_template('liga/rosters.html')
     context = {
      #   'owner_list': owner_list,
      #   'player_list': player_list,
         'roster_items': roster_items,
         'kevin': kevin,
+        'kevin_mcap': kevin_mcap,
+        'kevin_rcap': kevin_rcap,
     }
     return HttpResponse(template.render(context, request))
 
