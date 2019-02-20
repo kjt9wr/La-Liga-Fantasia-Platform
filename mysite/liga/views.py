b7ec0656c1d955810e71beb27441d394c8eb1b75
Kevin Thumim <kevinthumim@Kevins-MacBook-Pro.local>
2019-02-19 20:51:38 -0500

    remaining cap method

--- mysite/liga/views.py
+++ mysite/liga/views.py
@@ -16,21 +16,14 @@ def index(request):
 
 
 def rosters(request):
-    #owner_list = Owner.objects.all()
-    #player_list = Player.objects.filter(kept=True)
     roster_items = Roster.objects.all()
 
     kevin = Roster.objects.filter(owner__name='Kevin')
     kevin_mcap = Owner.objects.values('cap')[0]['cap']
-    temp = Roster.objects.filter(owner__name='Kevin').filter(athlete__kept=True)
-    sum = 0
-    for item in temp:
-        sum = sum + item.athlete.price
-    kevin_rcap = kevin_mcap - sum
+    kevin_rcap = remaining('Kevin', kevin_mcap)
+
     template = loader.get_template('liga/rosters.html')
     context = {
-     #   'owner_list': owner_list,
-     #   'player_list': player_list,
         'roster_items': roster_items,
         'kevin': kevin,
         'kevin_mcap': kevin_mcap,
@@ -55,4 +48,12 @@ def franchise(request):
         'player_list': player_list,
     }
     return HttpResponse(template.render(context, request))
+
+
+def remaining(name, max):
+    temp = Roster.objects.filter(owner__name=name).filter(athlete__kept=True)
+    sum = 0
+    for item in temp:
+        sum = sum + item.athlete.price
+    return max - sum
 
