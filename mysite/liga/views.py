2d0ee88ab0963dc6f07bd940cfacb02552506c17
Kevin Thumim <kevinthumim@Kevins-MacBook-Pro.local>
2019-02-27 21:00:20 -0500

    update kept

--- mysite/liga/views.py
+++ mysite/liga/views.py
@@ -1,10 +1,13 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 from .models import Owner
 from .models import Player
 from .models import Roster
 from django.template import loader
+from django.urls import reverse
+
+from liga.forms import LoginForm
 
 
 def index(request):
@@ -16,15 +19,16 @@ def index(request):
 def rosters(request, owner_id):
     roster_items = Roster.objects.all()
 
-    kevin = Roster.objects.filter(owner__pk=owner_id)
+    ownership = Roster.objects.filter(owner__pk=owner_id)
     owner_name = Owner.objects.filter(pk=owner_id).values('name')[0]['name']
     max_cap = Owner.objects.filter(pk=owner_id).values('cap')[0]['cap']
     rem_cap = remaining(owner_name, max_cap)
     context = {
         'roster_items': roster_items,
-        'kevin': kevin,
+        'ownership': ownership,
         'max_cap': max_cap,
         'owner_name': owner_name,
+        'owner_id': owner_id,
         'rem_cap': rem_cap,
     }
     return render(request, 'liga/rosters.html', context)
@@ -37,4 +41,16 @@ def remaining(name, max):
     for item in temp:
         sum = sum + item.athlete.price
     return max - sum
+
+
+def update(request, owner_id):
+    owner = Owner.objects.get(pk=owner_id)
+    selected_player = Player.objects.get(pk=request.POST['item'])
+    if selected_player.kept == False:
+        selected_player.kept = True
+    else:
+        selected_player.kept = False
+    selected_player.save()
+    return HttpResponseRedirect(reverse('liga:rosters', args=(owner.id,)))
+
 
