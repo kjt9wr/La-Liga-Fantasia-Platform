4a43bbec45fa2441acdad0a26e46985b20794a23
Kevin Thumim <kevinthumim@Kevins-MacBook-Pro.local>
2019-03-12 11:47:55 -0400

    cap moves

--- mysite/captracker/views.py
+++ mysite/captracker/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from liga.models import Owner
+from liga.models import Trade
 from liga.models import Player
 from liga.models import Roster
 from django.template import loader
@@ -12,10 +13,19 @@ from django.template import loader
 
 def captracker(request):
     owner_list = Owner.objects.all()
+    trades = Trade.objects.all()
+    cap_exchange = []
+    for trade in trades:
+        curr = store_trade(trade.tradeID, trade.recipient.pk, trade.giver.pk, trade.cap)
+        cap_exchange.append(curr)
+    print(cap_exchange)
     context = {
         'owner_list': owner_list,
+        'trades': trades,
+        'cap_exchange': cap_exchange,
     }
     return render(request, 'captracker/captracker.html', context)
+
 
 def viewTrade(request, tid):
     owner_list = Owner.objects.all()
@@ -33,3 +43,24 @@ def addTrade(request):
     }
     return render(request, 'captracker/addTrade.html', context)
 
+
+def store_trade(tid, f_owner_pk, s_owner_pk, cap):
+    f_owner = owner_recs(f_owner_pk, cap)
+    s_owner = owner_recs(s_owner_pk, cap*-1)
+    my_trade = [tid, f_owner, s_owner]
+    print(str(tid))
+    return my_trade
+
+
+def owner_recs(owner_pk, cap_received):
+    dict1 = {
+        'oid': owner_pk,
+        'cap_rec': cap_received,
+    }
+    return dict1
+
+# [{},{}]
+# {   oid: num
+#     cap rec: num
+#     players rec: [pid1,pid2...]
+#    }
