ecfb6dbd39c4ee9c0e843a640997ed6b6677d248
Kevin Thumim <kevinthumim@Kevins-MacBook-Pro.local>
2019-03-12 16:51:30 -0400

    cleaned navbar URL routing

--- mysite/captracker/urls.py
+++ mysite/captracker/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'captracker'
 urlpatterns = [
     path('', views.captracker, name='captracker'),
     path('trade/view/<tid>', views.viewTrade, name='viewTrade'),
