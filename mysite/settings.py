8d87efd2f7d40856038a207a8a7aa4c9052ff995
Kevin Thumim <kevinthumim@Kevins-MacBook-Pro.local>
2019-05-22 18:48:26 -0400

    changed allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,3 +125,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, 'liga/static/'),
 )
+
+ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1', 'protected-retreat-34900.herokuapp.com' ]
